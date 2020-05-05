3f9d1eadcfda4dbcf1fec02454876243c171afa0
Salim KAYABASI <salim.kayabasi@gmail.com>
2020-05-05 18:26:39 +0200

    Enabling scheduled timer

--- src/app/index.js
+++ src/app/index.js
@@ -1,28 +1,43 @@
-// const { getProducts, updatePrice } = require('./products');
-// const { checkPrice, checkStock } = require('./stock');
-// const prepare = require('./prepare');
-// const { getCustomers } = require('./customers');
-// const { notify } = require('./email');
-//
-// const { trigger } = require('./timer');
-//
-// const check = () => {
-//   prepare();
-//   const customers = getCustomers();
-//   const products = getProducts()
-//     .map((product) => ({
-//       ...product,
-//       ...checkStock(product),
-//     }))
-//     .filter(checkPrice)
-//     .map((product) => {
-//       notify(customers, product);
-//       return product;
-//     });
-//   updatePrice(products);
-// };
-//
-// module.exports = {
-//   trigger,
-//   check,
-// };
+const { getSheetData, setSheetValues } = require('./sheet');
+const { checkProduct } = require('./product');
+const { reply } = require('../telegram');
+
+const { trigger } = require('./timer');
+
+const check = () => {
+  let rows = getSheetData();
+  const map = rows.reduce((result, row) => {
+    const [, , , price, url] = row;
+    if (!result[url]) {
+      return Object.assign(result, { [url]: price });
+    }
+    return result;
+  }, {});
+
+  const updated = Object.entries(map).reduce((result, [url, p]) => {
+    const { price } = checkProduct(url);
+    if (price !== p) {
+      return Object.assign(result, { [url]: price });
+    }
+    return result;
+  }, {});
+  if (!Object.keys(updated).length) {
+    return;
+  }
+  rows = getSheetData();
+  setSheetValues(
+    rows.map((row) => {
+      const [user, id, name, price, url] = row;
+      const update = updated[url];
+      if (update) {
+        reply(id, `Price of *${name}* has changed to *${update}* from *${price}*`);
+      }
+      return [user, id, name, update || price, url];
+    }),
+  );
+};
+
+module.exports = {
+  trigger,
+  check,
+};
