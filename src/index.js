63bdf04ca7b5333a538fc4a3bdd8e768d555b786
Salim KAYABASI <salim.kayabasi@gmail.com>
2020-05-06 23:42:59 +0200

    clean up unused lines

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 const { check } = require('./check');
 const { prepareSheets } = require('./sheet');
-const { register } = require('./telegram');
 const { doPost, doGet } = require('./http');
-
-const prepare = () => {
-  prepareSheets();
-  register();
-};
 
 global.check = check;
 global.doPost = doPost;
 global.doGet = doGet;
-global.prepare = prepare;
+global.prepare = prepareSheets;
